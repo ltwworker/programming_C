a8c924bb4f6b52ab1edea1d132798a10d7796474
ltwworker <ltwworker@mail.ru>
2025-03-06 18:53:25 +1000

    Добавление кода выполняющего задания

--- laba3/main.c
+++ laba3/main.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+
+#define y 80
+
+int main() {
+    char str[y + 1];
+
+    printf("Vvedite stroky: ");
+    fgets(str, y + 1, stdin);
+    
+
+    int j;
+    for (j = 0; str[j]; j++) {
+        if (str[j] == 'a') str[j] = 'A';
+        else if (str[j] == 'b') str[j] = 'B';
+    }
+
+    printf("%s\n", str);
+
+    return 0;
+}
 
