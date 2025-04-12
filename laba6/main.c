97c4ba5af0110b4d269a9a167059825ac4ac2412
ltwworker <ltwworker@mail.ru>
2025-04-12 14:39:07 +1000

    Добавление выполненного задания

--- laba6/main.c
+++ laba6/main.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main() {
+    FILE *in, *out;
+    char fam[30], name[30], otch[30];
+    int year;
+    in = fopen("asd.txt","r");
+    out = fopen("asd2.txt","w");
+    while (fscanf(in, "%s %s %s %d", fam, &name, otch, &year) == 4) {
+        if (year > 1980  && name[0] == 'À' ) {
+            fprintf(out, "%s %s %s %d\n", fam, name, otch, year);
+        }
+    }
+    fclose(in);
+    fclose(out);
+    
+    printf("complete\n");
+    return 0;
+}
