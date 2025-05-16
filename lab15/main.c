81ffaffe7f3daf9b963575de20667dfb9c594ec2
ltwworker <ltwworker@mail.ru>
2025-05-16 14:57:32 +1000

    Add files via upload

--- lab15/main.c
+++ lab15/main.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int N,i;
+    printf("Vvedit num: ");
+    scanf("%d", &N);
+
+    int temp = N;
+    int digits[10];
+    int count = 0;
+
+    while (temp > 0) {
+        digits[count] = temp % 10;
+        temp /= 10;
+        count++;
+    }
+
+    for ( i = count - 1; i >= 0; i--) {
+        printf("%d", digits[i]);
+        if (i > 0) printf(" ");
+    }
+    printf("\n");
+
+    for ( i = 0; i < count; i++) {
+        printf("%d", digits[i]);
+        if (i < count - 1) printf(" ");
+    }
+    printf("\n");
+
+    return 0;
+}
 
