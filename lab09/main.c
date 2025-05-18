426ac72ce624bc0bfea553f60732695a7751cbd3
ltwworker <ltwworker@mail.ru>
2025-05-18 14:31:42 +1000

    Update main.c

--- lab09/main.c
+++ lab09/main.c
@@ -3,64 +3,51 @@
 #include <stdlib.h>
 
 int main() {
-    char input[256];
-    printf("Vvedite virazhenie: ");
-    fgets(input, sizeof(input), stdin);
-
-    double result = 0.0;
-    int sign = 1;
-    int i = 0;
-
-    while (input[i] == ' ') i++;
-
-    if (input[i] == '-') {
-        sign = -1;
-        i++;
-    } else if (input[i] == '+') {
-        sign = 1;
-        i++;
-    }
-
-    while (input[i] != '\0' && input[i] != '\n') {
-        while (input[i] == ' ') i++;
-
-        double num = 0.0;
-        int has_dot = 0;
-        double frac = 0.1;
-        int has_digit = 0;
-
-        while ((isdigit(input[i]) || input[i] == '.') && input[i] != '\0') {
-            if (input[i] == '.') {
-                has_dot = 1;
-                i++;
-                continue;
-            }
-            has_digit = 1;
-            if (!has_dot) {
-                num = num * 10 + (input[i] - '0');
-            } else {
-                num = num + (input[i] - '0') * frac;
-                frac /= 10;
-            }
-            i++;
-        }
-
-        result += sign * num;
-
-        while (input[i] == ' ') i++;
-
-        if (input[i] == '+') {
-            sign = 1;
-            i++;
-        } else if (input[i] == '-') {
-            sign = -1;
-            i++;
-        } else if (input[i] == '\0' || input[i] == '\n') {
-            break;
-        }
-    }
-
-    printf("Result: %.2lf\n", result);
+   char stroka[256];
+   
+   printf("Vvedite stroky: ");
+   scanf("%[^\n]",stroka);
+   
+   double result = 0.0;
+   char operator = '+';
+   
+   int i = 0;
+   while(stroka[i] != '\0'){
+   	if (stroka[i] == ' '){
+   		i++;
+   		continue;
+	   }
+	   
+	   if (stroka[i] == '+' || stroka[i] == '-'){
+	   	operator = stroka[i];
+	   	i++;
+	   	continue;
+	   }
+	   
+	   char num_stroka[50];
+	   int j = 0;
+	   
+	   if (stroka[i] == '-'){
+	   	
+	   	num_stroka[j++] = stroka[i++];
+	   	
+	   }
+	   
+	   while(isdigit(stroka[i]) || stroka[i] == '.'){
+	   	num_stroka[j++] = stroka[i++];
+	   }
+	   num_stroka[j] = '\0';
+	   
+	   double num = atof(num_stroka);
+	   
+	   if (operator == '+'){
+	   	result += num;
+	   } else if (operator == '-'){
+	   	result -= num;
+	   	
+	   }
+   }
+   
+   	printf("Result: %lf", result);
     return 0;
 }
-
