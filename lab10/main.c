1ae1641c6db3a543f3be65d4af219418869f970d
ltwworker <ltwworker@mail.ru>
2025-05-23 13:59:06 +1000

    Add files via upload

--- lab10/main.c
+++ lab10/main.c
@@ -1,45 +1,36 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-#define MAX(a,b) ((a) > (b) ? (a) : (b))
-int i;
-#define MAX_IN_ARRAY(arr, n, max) \
-    do { \
-        max = arr[0]; \
-        for (i = 1; i < n; i++) { \
-            if (arr[i] > max) { \
-                max = arr[i]; \
-            } \
-        } \
-    } while(0)
-
-int main() {
-    char input[256];
-    printf("Vvedite 4isla 4erez probel: ");
-    fgets(input, sizeof(input), stdin);
-
-    input[strcspn(input, "\n")] = '\0';
-
-    int numbers[100];
-    int count = 0;
-
-    char *token = strtok(input, " ");
-    while (token != NULL && count < 100) {
-        numbers[count++] = atoi(token);
-        token = strtok(NULL, " ");
-    }
-
-    if (count == 0) {
-        printf("Error\n");
-        return 1;
-    }
-
-    int max_value;
-    MAX_IN_ARRAY(numbers, count, max_value);
-
-    printf("Max number: %d\n", max_value);
-
-    return 0;
+int main(){
+	int num;
+	printf("Vvedite 4islo: ");
+	scanf("%d",&num);
+	
+	
+	int tsifri[30];
+	int count = 0;
+	
+	if (num == 0){
+		tsifri[count++] = 0;
+	
+	} else{
+		while (num > 0){
+			tsifri[count++] = num % 10;
+			num /= 10;
+		}
+	}
+	
+	int i;
+	
+	for(i = count - 1; i>=0; i--){
+		printf("%d",tsifri[i]);
+		if (i != 0) printf(" ");
+	}
+	printf(" => ");
+	
+	for(i = 0; i < count; i++ ){
+		printf("%d", tsifri[i]);
+		if (i != count - 1) printf(" ");
+	}
+	
+	return 0;
 }
-
