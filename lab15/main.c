cf8be38ac32238b2d025604b2df721a8071f609a
ltwworker <ltwworker@mail.ru>
2025-05-23 13:29:53 +1000

    Add files via upload

--- lab15/main.c
+++ lab15/main.c
@@ -1,32 +1,36 @@
 #include <stdio.h>
 
-int main() {
-    int N,i;
-    printf("Vvedit num: ");
-    scanf("%d", &N);
-
-    int temp = N;
-    int digits[10];
-    int count = 0;
-
-    while (temp > 0) {
-        digits[count] = temp % 10;
-        temp /= 10;
-        count++;
-    }
-
-    for ( i = count - 1; i >= 0; i--) {
-        printf("%d", digits[i]);
-        if (i > 0) printf(" ");
-    }
-    printf("\n");
-
-    for ( i = 0; i < count; i++) {
-        printf("%d", digits[i]);
-        if (i < count - 1) printf(" ");
-    }
-    printf("\n");
-
-    return 0;
+int main(){
+	int num;
+	printf("Vvedite 4islo: ");
+	scanf("%d",&num);
+	
+	
+	int tsifri[30];
+	int count = 0;
+	
+	if (num == 0){
+		tsifri[count++] = 0;
+	
+	} else{
+		while (num > 0){
+			tsifri[count++] = num % 10;
+			num /= 10;
+		}
+	}
+	
+	int i;
+	
+	for(i = count - 1; i>=0; i--){
+		printf("%d",tsifri[i]);
+		if (i != 0) printf(" ");
+	}
+	printf(" => ");
+	
+	for(i = 0; i < count; i++ ){
+		printf("%d", tsifri[i]);
+		if (i != count - 1) printf(" ");
+	}
+	
+	return 0;
 }
-
