19c274b74080f3f365632a21099b112a9602e41f
ltwworker <ltwworker@mail.ru>
2025-05-24 11:19:05 +1000

    Add files via upload

--- lab10/main.c
+++ lab10/main.c
@@ -3,24 +3,31 @@
 #include <string.h>
 
 #define MAX(arr, size) ({ \
-    int max_val = arr[0]; \
-    for (int i = 1; i < size; i++) { \
-        if (arr[i] > max_val) max_val = arr[i]; \
+	int i;\
+    int max = arr[0]; \
+    for (i = 1; i < size; i++) { \
+        if (arr[i] > max) max = arr[i]; \
     } \
-    max_val; \
+    max; \
+})
+
+#define ARIFM(arr, size) ({\
+    long sum = 0;\
+    int i;\
+    for (i = 0; i < size; i++) sum += (arr)[i];\
+    (double)sum / (size);\
 })
 
 int main() {
     char stroka[100];
     printf("Vvedite 4isla 4erez probel: ");
     fgets(stroka, sizeof(stroka), stdin);
-    stroka[strcspn(stroka, "\n")] = '\0';
 
     int numbers[100];
     int count = 0;
 
     char *razd = strtok(stroka, " ");
-    while (razd != NULL && count < 100) {
+    while (razd != NULL) {
         numbers[count++] = atoi(razd);
         razd = strtok(NULL, " ");
     }
@@ -31,6 +38,14 @@ int main() {
     } else {
         printf("Chisla ne vvedeny.\n");
     }
+    
+   if (count > 0){
+   	double arif = ARIFM(numbers,count);
+   	printf("sred arifm: %.12f\n", arif);
+   }
+    
+    
+    
 
     return 0;
 }
